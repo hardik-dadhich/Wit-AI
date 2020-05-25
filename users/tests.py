aad2eb4dc803f618bfae9c7bf8981611114c8dd0
Hardik dadhich <hadadhi1@in.ibm.com>
2020-05-26 01:31:08 +0530

    adding tests

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,9 @@
-from django.test import TestCase
-
 # Create your tests here.
+from django.contrib.auth.models import User
+from django.test import TestCase
+from django.utils import timezone
+
+
+class PostTestCase(TestCase):
+    def setUp(self):
+        print("will write up soon! ")
